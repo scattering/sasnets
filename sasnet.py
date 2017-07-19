596e50d7882d268128c00c0da2e105e2cc03fa51
Chris Wang <christopher.wang@nist.gov>
2017-07-19 16:49:58 -0400

    Fix SQL bugs, update network structure
    Fix bugs in SQL IO code in p2sql and sasnet.
    Begin process of retuing sasnet on larger model space.
    Add SQL generator train methods in sasnet.
    
    Signed-off-by: Chris Wang <christopher.wang@nist.gov>

--- sasnet.py
+++ sasnet.py
@@ -6,21 +6,21 @@ import itertools
 import logging
 import multiprocessing
 import os
-import random
 import re
 import sys
 import time
 
-import gc
 import keras
 import matplotlib.pyplot as plt
 import numpy as np
+import psycopg2 as psql
 import ruamel.yaml as yaml  # using ruamel for better input processing.
 from keras.callbacks import TensorBoard, EarlyStopping
 from keras.layers import Conv1D, Dropout, Flatten, Dense, \
     Embedding, MaxPooling1D
 from keras.models import Sequential
 from keras.utils.np_utils import to_categorical
+from psycopg2 import sql
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
@@ -35,6 +35,108 @@ parser.add_argument("-s", "--save-path",
 
 gpath = ""
 gpattern = ""
+
+DEC2FLOAT = psql.extensions.new_type( # May not be working
+    psql._psycopg.DECIMAL.values,
+    'DEC2FLOAT',
+    lambda value, curs: float(value) if value is not None else None)
+psql.extensions.register_type(DEC2FLOAT, None)
+
+
+def sql_dat_gen(dname, mname, dbname="sas_data", host="127.0.0.1", port="5673",
+                user="sasnets", encoder=None):
+    conn = psql.connect("dbname=" + dbname + " user=" + user + " host=" + host)
+    with conn:
+        with conn.cursor() as c:
+            c.execute("CREATE EXTENSION IF NOT EXISTS tsm_system_rows")
+            c.execute(
+                sql.SQL("SELECT * FROM {}").format(
+                    sql.Identifier(mname)))
+            x = np.asarray(c.fetchall())
+            # pprint(x)
+            q = x[0][1]
+            dq = x[0][2]
+            diq = x[0][3]
+            while True:
+                c.execute(
+                    sql.SQL(
+                        "SELECT * FROM {} TABLESAMPLE SYSTEM_ROWS(5)").format(
+                        sql.Identifier(dname)))
+                x = np.asarray(c.fetchall())
+                iq_list = x[:, 1]
+                y_list = x[:, 2]
+                encoded = encoder.transform(y_list)
+                yt = np.asarray(to_categorical(encoded, 71))
+                q_list = np.asarray(
+                    [np.transpose([q, iq, dq, diq]) for iq in iq_list])
+                yield q_list, yt
+    conn.close()
+
+
+def sql_net(dn, mn, verbosity=False, save_path=None, encoder=None, xval=None, yval=None):
+    if verbosity:
+        v = 1
+    else:
+        v = 0
+    base = None
+    sp = os.path.normpath(save_path)
+    if sp is not None:
+        if os.path.isdir(sp):
+            base = os.path.join(sp, str(time.time()))
+        else:
+            base = sp
+        if not os.path.exists(os.path.dirname(sp)):
+            os.makedirs(os.path.dirname(sp))
+    tb = TensorBoard(log_dir=os.path.dirname(base), histogram_freq=1)
+    es = EarlyStopping(min_delta=0.005, patience=5, verbose=v)
+
+    # Begin model definitions
+    model = Sequential()
+    # model.add(Embedding(4000, 128, input_length=267))
+    model.add(Conv1D(256, kernel_size=8, activation='relu', input_dim=4,
+                     input_length=267))
+    model.add(MaxPooling1D(pool_size=4))
+    model.add(Dropout(.17676))
+    model.add(Conv1D(256, kernel_size=6, activation='relu'))
+    model.add(MaxPooling1D(pool_size=3))
+    model.add(Dropout(.20782))
+    model.add(Flatten())
+    model.add(Dense(64, activation='tanh'))
+    model.add(Dropout(.20582))
+    model.add(Dense(71, activation='softmax'))
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    # Model Run
+    if v:
+        print(model.summary())
+    history = model.fit_generator(sql_dat_gen(dn, mn, encoder=encoder), 20000,
+                                  epochs=60, workers=1, verbose=v, validation_data=(xval, yval),
+                                  max_queue_size=1, callbacks=[tb, es])
+    score = None
+
+    # Model Save
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    if not (base is None):
+        with open(base + ".history", 'w') as fd:
+            fd.write(str(history.history) + "\n")
+            if score is not None:
+                fd.write(str(score) + "\n")
+        model.save(base + ".h5")
+        with open(base + ".svg", 'w') as fd:
+            plt.savefig(fd, format='svg', bbox_inches='tight')
+    if xval is not None and yval is not None:
+        score = model.evaluate_generator((xval, yval), 2000)
+        print('\nTest loss: ', score[0])
+        print('Test accuracy:', score[1])
+    logging.info("Complete.")
+
 
 
 # noinspection PyUnusedLocal
@@ -64,7 +166,7 @@ def read_parallel_1d(path, pattern='_eval_', typef='aggr', verbosity=False):
     """
     global gpath
     global gpattern
-    q_list, iq_list, y_list = (list() for i in range(3))
+    q_list, dq_list, iq_list, diq_list, y_list = (list() for i in range(5))
     # pattern = re.compile(pattern)
     n = 0
     nlines = None
@@ -86,7 +188,8 @@ def read_parallel_1d(path, pattern='_eval_', typef='aggr', verbosity=False):
         l = 0
         fn = os.listdir(path)
         chunked = [fn[i: i + 1] for i in xrange(0, len(fn), 1)]
-        pool = multiprocessing.Pool(multiprocessing.cpu_count() - 6, maxtasksperchild=2)
+        pool = multiprocessing.Pool(multiprocessing.cpu_count() - 6,
+                                    maxtasksperchild=2)
         result = np.asarray(
             pool.map(read_h, chunked, chunksize=1))
         pool.close()
@@ -122,11 +225,11 @@ def read_h(l):
                     q_list.extend([t2 for i in range(templ[1])])
                     iq_list.extend(ast.literal_eval(fd.readline().strip()))
             except Exception as e:
-                logging.warning(
-                    "skipped, {{0}}: {{1}}".format(e.errno, e.strerr))
+                logging.warning("skipped, " + str(e))
     return q_list, iq_list, y_list
 
 
+# noinspection PyCompatibility,PyUnusedLocal
 def read_seq_1d(path, pattern='_eval_', typef='aggr', verbosity=False):
     """
     Reads all files in the folder path. Opens the files whose names match the
@@ -145,7 +248,7 @@ def read_seq_1d(path, pattern='_eval_', typef='aggr', verbosity=False):
     :type typef: String
     :type verbosity: Boolean
     """
-    q_list, iq_list, y_list = (list() for i in range(3))
+    q_list, dq_list, iq_list, diq_list, y_list = (list() for i in range(5))
     pattern = re.compile(pattern)
     n = 0
     nlines = None
@@ -168,20 +271,23 @@ def read_seq_1d(path, pattern='_eval_', typef='aggr', verbosity=False):
                     with open(path + fn, 'r') as fd:
                         print("Reading " + fn)
                         templ = ast.literal_eval(fd.readline().strip())
-                        y_list.extend([templ[0] for i in range(templ[1])])
+                        y_list.extend([templ[0] for i in xrange(templ[1])])
                         t2 = ast.literal_eval(fd.readline().strip())
-                        q_list.extend([t2 for i in range(templ[1])])
+                        q_list.extend([t2 for i in xrange(templ[1])])
                         iq_list.extend(ast.literal_eval(fd.readline().strip()))
+                        dqt = ast.literal_eval(fd.readline().strip())
+                        dq_list.extend([dqt for i in xrange(templ[1])])
+                        diqt = ast.literal_eval(fd.readline().strip())
+                        diq_list.extend([diqt for i in xrange(templ[1])])
                         nlines += templ[1]
                     if (n % 1000 == 0) and verbosity:
                         print("Read " + str(nlines) + " points.")
                 except Exception as e:
-                    logging.warning(
-                        "skipped, " + str(e))
+                    logging.warning("skipped, " + str(e))
     else:
         print("Error: the type " + typef + " was not recognised. Valid types "
                                            "are 'aggr' and 'json'.")
-    return q_list, iq_list, y_list, nlines
+    return q_list, iq_list, y_list, dq_list, diq_list, nlines
 
 
 def plot(q, i_q):
@@ -242,11 +348,11 @@ def oned_convnet(x, y, xevl=None, yevl=None, random_s=235, verbosity=False,
 
     # Begin model definitions
     model = Sequential()
-    model.add(Embedding(3000, 64, input_length=xval.shape[1]))
-    model.add(Conv1D(128, kernel_size=3, activation='relu'))
+    model.add(Embedding(4000, 128, input_length=xval.shape[1]))
+    model.add(Conv1D(128, kernel_size=6, activation='relu'))
     model.add(MaxPooling1D(pool_size=4))
     model.add(Dropout(.17676))
-    model.add(Conv1D(64, kernel_size=3, activation='relu'))
+    model.add(Conv1D(64, kernel_size=6, activation='relu'))
     model.add(MaxPooling1D(pool_size=4))
     model.add(Dropout(.20782))
     model.add(Flatten())
@@ -335,19 +441,50 @@ def trad_nn(x, y, xevl=None, yevl=None, random_s=235):
 
 def main(args):
     parsed = parser.parse_args(args)
-    time_start = time.clock()
-    a, b, c, n = read_seq_1d(parsed.path, pattern='_all_',
-                                  verbosity=parsed.verbose)
-    #gc.collect()
-    at, bt, ct, dt = read_seq_1d(parsed.path, pattern='_eval_',
-                                      verbosity=parsed.verbose)
-    time_end = time.clock() - time_start
-    logging.info("File I/O Took " + str(time_end) + " seconds for " + str(n) +
-                 " points of data.")
-    r = random.randint(0, 2 ** 32 - 1)
-    logging.warn("Random seed for this iter is " + str(r))
-    oned_convnet(np.asarray(b), c, np.asarray(bt), ct, random_s=r,
-                 verbosity=parsed.verbose, save_path=parsed.save_path)
+    # time_start = time.clock()
+    # a, b, c, d, e, n = read_seq_1d(parsed.path, pattern='_all_',
+    #                               verbosity=parsed.verbose)
+    # gc.collect()
+    # at, bt, ct, dt, et, nt = read_seq_1d(parsed.path, pattern='_eval_',
+    #                                     verbosity=parsed.verbose)
+    # time_end = time.clock() - time_start
+    # logging.info("File I/O Took " + str(time_end) + " seconds for " + str(n) +
+    #             " points of data.")
+    # r = random.randint(0, 2 ** 32 - 1)
+    # logging.warn("Random seed for this iter is " + str(r))
+    # oned_convnet(np.asarray(b), c, np.asarray(bt), ct, random_s=r,
+    #             verbosity=parsed.verbose, save_path=parsed.save_path)
+    conn = psql.connect("dbname=sas_data user=sasnets host=127.0.0.1")
+    # conn.set_session(readonly=True)
+    with conn:
+        with conn.cursor() as c:
+            c.execute("SELECT model FROM train_data;")
+            xt = set(c.fetchall())
+            y = [i[0] for i in xt]
+            encoder = LabelEncoder()
+            encoder.fit(y)
+            c.execute("CREATE EXTENSION IF NOT EXISTS tsm_system_rows")
+            c.execute(
+                    sql.SQL("SELECT * FROM {}").format(
+                        sql.Identifier("train_metadata")))
+            x = np.asarray(c.fetchall())
+            q = x[0][1]
+            dq = x[0][2]
+            diq = x[0][3]
+            c.execute(sql.SQL(
+                            "SELECT * FROM {} TABLESAMPLE SYSTEM_ROWS(10000)").format(
+                            sql.Identifier("eval_data")))
+            x = np.asarray(c.fetchall())
+            iq_list = x[:, 1]
+            y_list = x[:, 2]
+            encoded = encoder.transform(y_list)
+            yt = np.asarray(to_categorical(encoded, 71))
+            q_list = np.asarray([np.transpose([q, iq, dq, diq]) for iq in iq_list])
+    # print(sorted(y))
+
+    sql_net("train_data", "train_metadata",
+            verbosity=parsed.verbose, save_path=parsed.save_path,
+            encoder=encoder, xval=q_list, yval=yt)
 
 
 if __name__ == '__main__':
