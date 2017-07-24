8ac9d933e6111472788067933a23c6aeeb00b0e6
Kisa (Chris) Ayano <kisa.ayano@icloud.com>
2017-07-24 14:53:13 -0400

    Update docs conf to fix build bugs
    Signed-off-by: Chris Wang <christopher.wang@nist.gov>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='sasnets',
+        version='0.1a',
+        description='CNN classifier for SANS data',
+        author='Chris Wang',
+        author_email='wangch00@icloud.com',
+        packages=['./',],
+        )
+
