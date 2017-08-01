86eab377a0b0ff3f3705505c6ac5ad81a344994b
Chris Wang <kisa.ayano@icloud.com>
2017-08-01 10:14:33 -0400

    Update doc config.
    Signed-off-by: Chris Wang <wangch00@icloud.com>

--- docs/conf.py
+++ docs/conf.py
@@ -18,9 +18,10 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('../../'))
-sys.path.append(os.path.abspath('../'))
-sys.path.append(os.path.abspath('../sasnets/'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../sasnets/'))
+sys.path.insert(0, os.path.abspath('../sasnets/util/'))
 #sys.path.append('/home/chwang/svenv/lib/python2.7/site-packages/')
 #sys.path.append('/home/chwang/svenv/lib/python2.7/dist-packages/')
 #sys.path.append('/usr/lib/python2.7/dist-packages/')
