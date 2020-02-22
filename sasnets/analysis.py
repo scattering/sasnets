026dbf02b63d665e75b368875e217747249f9777
Paul Kienzle <pkienzle@nist.gov>
2020-02-22 02:10:42 -0500

    statistics on credible sets, rank and rejection; reorder confusion matrix to match dendrogram labels

--- sasnets/analysis.py
+++ sasnets/analysis.py
@@ -54,6 +54,23 @@ def predict_and_val(classifier, x, y, categories):
     Runs the classifier on the input datasets and compares the results with the
     correct labels provided from y.
 
+    One reported statistic is the frequency with which the target value lies
+    within the 95% credible set.  The "shortest credible set" for a prediction
+    is computed by sorting the probabilities from lowest to highest, resulting
+    in a cumulative mass function (cmf).  If the cmf value associated the
+    target bin is above 5% then the target is said to be in the
+    "95% credible set".
+
+    The "average credibility" results from accumulating the individual cmfs
+    for each target and dividing by the number of times the target appears.
+    If the target is always the most probable, then its cmf will always
+    be 100% and so that will be the average credibility.  If there is 2-way
+    confusion between targets (say 75/25 split), then the first will be 100%
+    with freq 75% and somewhere between 0% and 50% the remaining 25% of
+    the time, leading to an "average credibility" somewhat higher than 75%.
+    Similarly for the 25% case.  So a highly biased and perhaps not very
+    useful statistic.  At least order should be maintained.
+
     :param classifier: The trained classifier.
     :param x: List of x values to predict on.
     :param y: List of y values to predict on.
@@ -63,33 +80,69 @@ def predict_and_val(classifier, x, y, categories):
     encoder = OnehotEncoder(categories)
     yindex = encoder.index(y)
     prediction = classifier.predict(fix_dims(x))
-    error_frequency = {name: 0 for name in categories}
-    top5_frequency = {name: 0 for name in categories}
+    error_freq = {name: 0 for name in categories}
+    top5_freq = {name: 0 for name in categories}
+    rank_avg = {name: 0 for name in categories}
+    cred_68 = {name: 0 for name in categories}
+    cred_95 = {name: 0 for name in categories}
+    cred_avg = {name: 0 for name in categories}
     freq = {name: 0 for name in categories}
+    num_rejected = {name: 0 for name in categories}
     errors = []
     for k, (prob, actual, actual_index) in enumerate(zip(prediction, y, yindex)):
+        # Sort by increasing probability
         sort_index = prob.argsort(axis=-1)
+        # Count frequency of targets for normalization.
         freq[actual] += 1
-        if actual_index in sort_index[-5:]:
-            top5_frequency[actual] += 1
-        mle = prob.argmax(axis=-1)
-        predicted = encoder.label([mle])[0]
-        if predicted != actual:
+        # Generate cmf for each bin by sorting and accumulating.
+        cdf = np.empty_like(prob)
+        cdf[sort_index] = np.cumsum(prob[sort_index])
+        # Track statistics for the frequency and average credible interval.
+        cred_68[actual] += (cdf[actual_index] >= 1 - 0.68)
+        cred_95[actual] += (cdf[actual_index] >= 1 - 0.95)
+        cred_avg[actual] += cdf[actual_index]
+        # Track presence in top-5 choices (arbitrary, but interesting from
+        # a blind fitting perspective)
+        top5_freq[actual] += (actual_index in sort_index[-5:])
+        # Track rank by finding the position of actual in the sorted list.
+        # Need to reverse the list since lowest to highest, and add one since
+        # array indices are zero-origin. where()[0][0] because where returns
+        # a tuple of dimensions with each dimension listing the position
+        # along that dimension containing the item.
+        rank_avg[actual] += np.where(sort_index[::-1] == actual_index)[0][0] + 1
+        # Count the number below the 95% probability level
+        num_rejected[actual] += np.sum(cdf < 0.05)
+        # Check if prediction from mle matches actual. Note that the maximum
+        # likelihood is at the end of the sorted list of probabilities.
+        mle_index = sort_index[-1]
+        if mle_index != actual_index:
+            predicted = encoder.label([mle_index])[0]
             try:
-                ratio = prob[actual_index] / prob[mle]
+                ratio = prob[actual_index] / prob[mle_index]
             except IndexError:
-                print(actual, actual_index, mle, predicted, len(categories), prob)
+                print(actual, actual_index, mle_index, predicted, len(categories), prob)
                 raise
             if len(errors) == 20:
                 print("...")
             if len(errors) < 20:
                 print(f"Predicted: {predicted}, Actual: {actual}, Index: {k}, Target/mle: {ratio:.2}.")
-            error_frequency[actual] += 1
+            error_freq[actual] += 1
             errors.append((k, predicted, actual, ratio))
-    print("Error rate")
-    print(columnize(f"{k}: {int(100*v/freq[k]+0.5)}%" for k, v in sorted(error_frequency.items())))
-    print("Top 5 rate")
-    print(columnize(f"{k}: {int(100*v/freq[k]+0.5)}%" for k, v in sorted(top5_frequency.items())))
+    #print("Error rate")
+    #print(columnize(f"{k}: {int(100*v/freq[k]+0.5)}%" for k, v in sorted(error_freq.items())))
+    #print("Top 5 rate")
+    #print(columnize(f"{k}: {int(100*v/freq[k]+0.5)}%" for k, v in sorted(top5_freq.items())))
+    print("Average rank")
+    print(columnize(f"{k}: {v/freq[k]:.1f}" for k, v in sorted(rank_avg.items())))
+    print("Average number rejected at the 5% level")
+    print(columnize(f"{k}: {v/freq[k]:.1f}" for k, v in sorted(num_rejected.items())))
+    print("68% CI rate")
+    print(columnize(f"{k}: {int(100*v/freq[k]+0.5)}%" for k, v in sorted(cred_68.items())))
+    #print("95% CI rate")
+    #print(columnize(f"{k}: {int(100*v/freq[k]+0.5)}%" for k, v in sorted(cred_95.items())))
+    print("Average cred")
+    print(columnize(f"{k}: {int(100*v/freq[k]+0.5)}%" for k, v in sorted(cred_avg.items())))
+
     rows, predicted, _, _ = zip(*errors)
     return rows, predicted
 
@@ -206,14 +259,17 @@ def plot_dendrogram(classifier, x, y, categories):
     """
     import matplotlib.pyplot as plt
     arr = confusion_matrix(classifier, x, y, categories)
-    plt.subplot(211)
-    plt.pcolor(arr, cmap='RdBu')
-    plt.gca().get_xaxis().set_visible(False)
-    plt.gca().get_yaxis().set_visible(False)
     plt.subplot(212)
     z = linkage(arr, 'average')
     h = dendrogram(z, leaf_rotation=90., leaf_font_size=8, labels=categories,
                    color_threshold=.5, get_leaves=True)
+    plt.gca().get_yaxis().set_visible(False)
+    plt.subplot(211)
+    # Reorder array rows and columns to match dendrogram order
+    reorder = arr[h['leaves'], :][:, h['leaves']]
+    plt.pcolor(reorder, cmap='RdBu')
+    #plt.pcolor(np.log10(reorder), cmap='RdBu')
+    plt.gca().get_xaxis().set_visible(False)
     plt.gca().get_yaxis().set_visible(False)
     plt.tight_layout()
     plt.show()
